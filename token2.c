b5185c44368aaef6753d3bbad47cba20b1d8e245
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2017-11-14 14:40:15 +0900

    token&token2 merge

--- token2.c
+++ token2.c
@@ -9,8 +9,7 @@ char token[20];
 char res[13][20]={"begin","end","if","then","while","do",
 				  "return","function","var","const","odd",
 				  "write","writeln"};
-char sign[17][10]={"+","-","*","/","(",")","=","<",">",
-			      "<>","<=",">=",",",".",";",":="};
+char sign[10]={'+','-','*','/','(',')','=',',','.',';'};
 FILE *fi;
 //関数宣言
 void nexttoken(int);  //次のトークンを読み込む関数
@@ -23,7 +22,11 @@ void backtoken();  //現在のトークンの番号を1つ前に戻す
 void block(), varDecl(), constDecl(), funcDecl(),
 	 statement(), condition(), expression(), term(),
 	 factor();              //構文規則を指定
+
 void get_token();
+int check_word(char);
+int check_sign_1(char);
+int check_token(char*);
 
 int main(int argc, char **argv)
 {
@@ -239,7 +242,7 @@ int ttype()  //トークンのタイプを返す
 	}
 
 	for(i=0;i<16;i++){
-		c=strcmp(token,sign[i]);
+		c=strcmp(token,sign);
 		if(c==0){
 			return 2;
 			break;
@@ -264,11 +267,112 @@ void exit_func(char str[20])  //文法規則に合わないため終了
 	exit(1);  //強制終了
 }
 
-
 void get_token()
 {
-	//1週目で行った内容
+	int i,j,token_num,word;
+	char c;
+	fi=fopen("program1","r");
+	token_num=0;  //トークンの数の初期化
+	j=0;
+	while(1){
+		if(j==0)word=0;
+		c=fgetc(fi);  //1文字ずつ入力
+		printf("%c\n",c);
+		if(c==EOF)break;
 
-
+		if(check_word(c)==0){//数字なら 
+			outtoken[token_num][j]=c;
+			j++;
+		}
+		else if(check_word(c)==1){//文字なら 
+			if(j!=0 && word==0){
+				token_num++;
+				j=0;
+			}
+			word=1;
+			outtoken[token_num][j]=c;
+			j++;
+		}
+		else if(check_word(c)==2){  //改行，スペース，タブなら
+			if(j!=0){
+				outtoken[token_num][j]='\0';
+				j=0;
+				token_num++;
+			}
+			else{
+				//do nothing!!
+			}
+		}else if(check_word(c)==3){  //記号なら(1文字)
+			if(j!=0)token_num++;
+			outtoken[token_num][0]=c;
+			token_num++;
+			j=0;
+		} else if(check_word(c)==4){  //記号なら(2文字)
+			if(j!=0)token_num++;
+			outtoken[token_num][0]=c;
+			outtoken[token_num][1]=fgetc(fi);
+			token_num++;
+			j=0;
+		}
+	}
+	//output	
+	for(i=0;i<token_num;i++){
+		switch(check_token(outtoken[i])){
+			case 0:
+				printf("%3d  %s <整数>\n",i,outtoken[i]);
+				break;
+			case 1:
+				printf("%3d  %s <記号>\n",i,outtoken[i]);
+				break;
+			case 2:
+				printf("%3d  %s <予約語>\n",i,outtoken[i]);
+				break;
+			case 3:
+				printf("%3d  %s <名前>\n",i,outtoken[i]);
+				break;
+			default:
+				break;
+		}
+	}
 }
 
+int check_word(char c)
+{
+	if(c>='0' && c<='9') // 数字なら 
+		return 0;
+	else if(c>='a' && c<='z')        //アルファベット
+		return 1;
+	else if(c=='\n' || c==' ' || c=='\t')        //改行，スペース，タブ
+		return 2;
+	else if(check_sign_1(c))        //1文字のみの記号
+		return 3;
+	else if(c==':')      //コロンは'='と必ず一緒になる
+		return 4;
+	else if(c=='<' || c=='>'){  //この場合は次の文字を見る必要がある
+		c=fgetc(fi);
+		fseek(fi,-1,1);   //ファイルポインタを1つ戻す 
+		if(c=='=' || c=='>'){
+			return 4;
+		}else{
+			return 3;
+		}
+	}
+}
+
+int check_sign_1(char c){
+	int i;
+	for (i=0;i<10;i++) if(c==sign[i])return 1;
+	return 0;
+}
+
+int check_token(char *token){
+	int i;
+	if(token[0]>='0' && token[0]<='9')return 0;//整数
+	else if(check_sign_1(token[0])==1 || token[0]==':' || token[0]=='<'|| token[0]=='>')return 1;//記号
+	else{   
+		for(i=0;i<13;i++){
+			if(strcmp(token,res[i])==0)return 2;//予約語
+		}
+		return 3;//名前
+	}
+}
